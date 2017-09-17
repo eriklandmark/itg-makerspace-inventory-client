8ef61acb19e323779ae578a72a570afc6c47843c
itggot-erik-landmark <erik.landmark@itggot.se>
2017-09-18 00:32:34 +0200

    Fixade så att appen kan kopla upp sig på local server

--- itg-makerspace-inventory/src/itg/makerspace/authentication/AuthenticationManager.java
+++ itg-makerspace-inventory/src/itg/makerspace/authentication/AuthenticationManager.java
@@ -3,6 +3,7 @@ package itg.makerspace.authentication;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -14,11 +15,13 @@ public class AuthenticationManager {
 	
 	public String login(String email, String password) {
 		try {
-			String httpsURL = "https://itg-makerspace.herokuapp.com/auth";
+			
+			//String httpsURL = "https://itg-makerspace.herokuapp.com/auth";
+			String httpsURL = "http://192.168.1.216:9292/auth";
 			String query = "email=" + URLEncoder.encode(email,"UTF-8") + "&" + "password=" + URLEncoder.encode(password,"UTF-8");
 	
 			URL myurl = new URL(httpsURL);
-			HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+			HttpURLConnection con = (HttpURLConnection)myurl.openConnection();
 			con.setRequestMethod("POST");
 	
 			con.setRequestProperty("Content-length", String.valueOf(query.length())); 
@@ -49,11 +52,12 @@ public class AuthenticationManager {
 	
 	public boolean sendNewLoan(int user_id, String auth_key, String items) {
 		try {
-			String httpsURL = "https://itg-makerspace.herokuapp.com/new-loan";
+			//String httpsURL = "https://itg-makerspace.herokuapp.com/new-loan";
+			String httpsURL = "http://192.168.1.216:9292/new-loan";
 			String query = "user_id=" + URLEncoder.encode(String.valueOf(user_id), "UTF-8") + "&security_key=" + URLEncoder.encode(auth_key,"UTF-8") + "&items=" + URLEncoder.encode(items,"UTF-8");
 	
 			URL myurl = new URL(httpsURL);
-			HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+			HttpURLConnection con = (HttpURLConnection)myurl.openConnection();
 			con.setRequestMethod("POST");
 	
 			con.setRequestProperty("Content-length", String.valueOf(query.length())); 
